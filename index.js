b34d1fc78ce7d91629718b8fd95261091616e2ed
HasanMt12 <hasan.2mahmud22gmail.com>
2022-10-25 19:24:45 +0600

    client req courses list routes added

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 5000;
 
+const list = require('./CustomFakeDb/CoursesList.json')
+
 app.get('/', (req, res) => {
     res.send('E-learning API running')
 })
 
+app.get('/courses-list', (req, res) => {
+    res.send(list)
+})
 app.listen(port, () => {
     console.log('E-learning web server running in port',port);
 })
